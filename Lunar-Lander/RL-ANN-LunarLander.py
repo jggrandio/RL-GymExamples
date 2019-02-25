114a2dbcb15fb375e7d4b8bf46de8a19319e0b4f
Javier Grandio <javiergrandiogonzalez@gmail.com>
2019-02-25 10:26:51 +0100

    cartpole better

--- Lunar-Lander/RL-ANN-LunarLander.py
+++ Lunar-Lander/RL-ANN-LunarLander.py
@@ -9,12 +9,10 @@ from keras.optimizers import Adam
 
 EPISODES = 1000
 
-GANMA = 0.99    # discount rate
+GANMA = 0.999    # discount rate
 EPSILON = 1.0  # exploration rate
-#Works with min 0.01
 EPSILON_MIN = 0.00
-EPSILON_DECAY = 0.995
-# works with learning rate 0.001
+EPSILON_DECAY = 0.996
 LEARNING_RATE = 0.001
 
 #TRAIN = 1 trains to a file. TRAIN = 0 plays with the coefficients of the file.
@@ -22,34 +20,35 @@ TRAIN = 1;
 FILE_NAME = "ann-weights.h5"
 
 if __name__ == "__main__":
-  env = gym.make('CartPole-v1')
-  state_size = 4
+  env = gym.make('LunarLander-v2')
+  state_size = 8
   action_size = env.action_space.n
 
   #Create the artificial neural network (ann)
   ann = Sequential()
 
-  ann.add(Dense(24, input_dim = state_size, activation='relu'))
-  ann.add(Dense(24, activation='relu'))
-  ann.add(Dense(2, activation='linear'))
+  ann.add(Dense(64, input_dim = state_size, activation='relu'))
+  ann.add(Dense(64, activation='relu'))
+  ann.add(Dense(action_size, activation='linear'))
   ann.compile(loss='mse', optimizer=Adam(LEARNING_RATE), metrics=['mae'])
 
   if TRAIN == 1:
     epsilon = EPSILON
     for e in range(EPISODES):
       #Initial state and action
+      recomp = 0
       state = env.reset()
-      state = np.reshape(state[0:4], [1, state_size])
+      state = np.reshape(state[0:state_size], [1, state_size])
       epsilon *= EPSILON_DECAY;
       done = False
       time = 0
       #choose greedy action
       action = np.argmax(ann.predict(state))
       while not done:
-        # env.render()
+        #env.render()
         # Take that action and see the next state and reward
         new_state, reward, done, _ = env.step(action)
-        new_state = np.reshape(new_state[0:4], [1, state_size])
+        new_state = np.reshape(new_state[0:state_size], [1, state_size])
         if done:
           target = reward
           target_vec = ann.predict(state)
@@ -72,32 +71,33 @@ if __name__ == "__main__":
         state = new_state
         action = new_action
         # Count the time
-        time += 1
+        recomp += reward
 
       #print episode results
       print("episode: {}/{}, score: {}, e: {:.2}"
-            .format(e, EPISODES, time, epsilon))
+            .format(e, EPISODES, recomp, epsilon))
 
 
     for e in range(EPISODES):
         # Initial state and action
         state = env.reset()
-        state = np.reshape(state[0:4], [1, state_size])
+        state = np.reshape(state[0:state_size], [1, state_size])
         done = False
         time = 0
+        recomp = 0
         # choose greedy action
         action = np.argmax(ann.predict(state))
         while not done:
             env.render()
             # Take that action and see the next state and reward
             new_state, reward, done, _ = env.step(action)
-            new_state = np.reshape(new_state[0:4], [1, state_size])
+            new_state = np.reshape(new_state[0:state_size], [1, state_size])
             # Select an new action (epsilon-greedy)
             new_action = np.argmax(ann.predict(new_state)[0])
             # Train the model if the episode finished
             state = new_state
             action = new_action
             # Count the time
-            time += 1
+            recomp += reward
         # print episode results
-        print("episode: {}/{}, score: {},".format(e, EPISODES, time ))
+        print("episode: {}/{}, score: {},".format(e, EPISODES, recomp ))
